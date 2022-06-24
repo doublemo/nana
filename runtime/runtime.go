dd4cb6eb9798ad75f54f57cf40b76ee5a0f27143
randyma <435420057@qq.com>
2022-06-24 13:14:04 +0800

    runtime

---
+++ runtime/runtime.go
@@ -0,0 +1,56 @@
+// Copyright (c) 2022 The Linna Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package runtime
+
+// 定义运行时常量
+const (
+	// All available environmental variables made available to the runtime environment.
+	// This is useful to store API keys and other secrets which may be different between servers run in production and in development.
+	//   envs := ctx.Value(runtime.RUNTIME_CTX_ENV).(map[string]string)
+	// This can always be safely cast into a `map[string]string`.
+	RUNTIME_CTX_ENV = "env"
+
+	// Http headers. Only applicable to HTTP RPC requests.
+	RUNTIME_CTX_HEADERS = "headers"
+
+	// Query params that was passed through from HTTP request.
+	RUNTIME_CTX_QUERY_PARAMS = "query_params"
+
+	// The user ID associated with the execution context.
+	RUNTIME_CTX_USER_ID = "user_id"
+
+	// The username associated with the execution context.
+	RUNTIME_CTX_USERNAME = "username"
+
+	// Variables stored in the user's session token.
+	RUNTIME_CTX_VARS = "vars"
+
+	// The user session expiry in seconds associated with the execution context.
+	RUNTIME_CTX_USER_SESSION_EXP = "user_session_exp"
+
+	// The user session associated with the execution context.
+	RUNTIME_CTX_SESSION_ID = "session_id"
+
+	// The user session's lang value, if one is set.
+	RUNTIME_CTX_LANG = "lang"
+
+	// The IP address of the client making the request.
+	RUNTIME_CTX_CLIENT_IP = "client_ip"
+
+	// The port number of the client making the request.
+	RUNTIME_CTX_CLIENT_PORT = "client_port"
+)
+
+type Initializer interface{}
